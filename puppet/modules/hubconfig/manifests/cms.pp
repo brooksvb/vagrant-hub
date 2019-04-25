5d71ea9a835d7a32e75d38a8d91711ee89fa62fc
Brooks Van Buren <mail@brooks.codes>
2019-04-25 18:29:14 -0400

    Add database seeding and dependencies

--- puppet/modules/hubconfig/manifests/cms.pp
+++ puppet/modules/hubconfig/manifests/cms.pp
@@ -17,6 +17,7 @@ class hubconfig::cms {
 	$breadcrumb = '/etc/.hubzero_cms_first_time_setup' # path to breadcrumb
 	$db_user_breadcrumb = '/etc/.hubzero_db_usr_first_time_setup'
 	$db_create_breadcrumb = '/etc/.hubzero_db_create_first_time_setup'
+	$db_seed_breadcrumb = '/etc/.hubzero_db_seed_first_time_setup'
 
 	exec { 'clone cms':
 		require => [Package['git'], File['/var/www/dev']],
@@ -87,6 +88,7 @@ class hubconfig::cms {
 	}
 
 	exec { 'mysql db create':
+		require => Exec['mysql user create'],
 		before => [File["${db_create_breadcrumb}"], File["${breadcrumb}"]],
 		# TODO: Temporarily removed password, see above: -p94B5FQN3fW8qZ4
 		command => "/usr/bin/mysql -u root --execute=\"
@@ -99,6 +101,25 @@ class hubconfig::cms {
 
 	file { "${db_create_breadcrumb}":
 		path => "${db_create_breadcrumb}",
+		ensure => present,
+		owner => 'root',
+		group => 'root',
+		mode => '0111'
+	}
+
+	# Create tables, seed data
+	exec { 'mysql db seed':
+		require => Exec['mysql db create'],
+		before => [File["${db_seed_breadcrumb}"], File["${breadcrumb}"]],
+		# TODO: Temporarily removed password, see above: -p94B5FQN3fW8qZ4
+		command => "/usr/bin/mysql -u root \
+			< /vagrant/puppet/modules/hubconfig/files/bin/setup.sql
+		",
+		creates => ["${db_seed_breadcrumb}", "${breadcrumb}"]
+	}
+
+	file { "${db_seed_breadcrumb}":
+		path => "${db_seed_breadcrumb}",
 		ensure => present,
 		owner => 'root',
 		group => 'root',
@@ -131,7 +152,7 @@ class hubconfig::cms {
 
 	exec { 'muse setup':
 		before => File["${breadcrumb}"],
-		require => Exec['clone cms'],
+		require => [Exec['clone cms'], Exec['mysql db seed']],
 		cwd => '/var/www/dev',
 		command => '/usr/bin/php5 ./muse migration -i -f',
 		creates => "${breadcrumb}"
