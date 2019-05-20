bd1aa4a3e3f1701f56903f8104052b4b59307975
Brooks Van Buren <mail@brooks.codes>
2019-05-20 15:37:00 -0400

    Remove original hubconfig module

--- puppet/environments/production/modules/hubconfig/files/bin/muse.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mysqlcheck --repair --all-databases -p 94B5FQN3fW8qZ4
-echo "running muse"
-sleep 5
-cd /var/www/dev/
-php5 ./muse migration -i -f
-echo "done"
--- puppet/environments/production/modules/hubconfig/files/bin/seed.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "seeding mysql"
-/usr/bin/mysqladmin -u root -h localhost password '94B5FQN3fW8qZ4'
-mysql -u root -p94B5FQN3fW8qZ4 --execute="CREATE USER 'example'@'localhost' IDENTIFIED BY 'YMx7ZE35jw45f9';"
-mysql -u root -p94B5FQN3fW8qZ4 --execute="GRANT ALL PRIVILEGES ON example.* TO 'example'@'localhost';"
-mysql -u root -p94B5FQN3fW8qZ4 --execute="CREATE DATABASE example;"
-mysql -u root -p94B5FQN3fW8qZ4 example < /config/bin/setup.sql
